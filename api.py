bed67472c7d2efe0e326a1d71639b1bda22ab449
Muskan Patel <57407831+muskan7860@users.noreply.github.com>
2026-02-11 12:22:34 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,10 @@
+Each 5-minute candle contains:
+
+Tag	Meaning
+1. open	Price at start of 5-min
+2. high	Highest price in 5-min
+3. low	Lowest price in 5-min
+4. close	Price at end of 5-min
+5. volume	Number of shares traded
+
+👉 These are standard stock market terms, not Python terms.
