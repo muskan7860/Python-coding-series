d7edd073451df33623af68ba69e0b64fd6b22ae7
Muskan Patel <57407831+muskan7860@users.noreply.github.com>
2026-02-11 21:41:38 +0530

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -6,3 +6,20 @@ params = {
 }
 response = requests.get(url, params=params)
 print(response.json())
+==========================================================================================
+You will create a Python script that:
+
+Calls a public API (example: GitHub API, JSONPlaceholder) list
+Fetches data using the requests library
+Parses the JSON response
+Extracts meaningful information from the response
+Prints the processed output to the terminal
+Saves the processed data into a JSON file
+This task helps you understand how DevOps engineers work with APIs and structured data.
+
+Expected Output
+One Python script (example: api_data_fetcher.py)
+One JSON output file (example: output.json)
+Output should be visible:
+In terminal
+In the JSON file
